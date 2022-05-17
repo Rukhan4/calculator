cb61363caac60e8c049f145f1fd115a6c5ed12c0
Surur Khan <surur.rkhan@gmail.com>
2022-05-17 18:27:22 -0400

    Created function to perform operations imported from a separate file

---
+++ ops.js
@@ -0,0 +1,22 @@
+let add = (a, b) => a + b;
+
+let subtract = (a, b) => a - b;
+
+let multiply = (a, b) => a * b;
+
+let divide = (a, b) => a / b;
+
+export function operate(symbol, a, b) {
+    switch (symbol) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+import { operate } from './ops.js';
+
+// console.log(operate('-', 3, 5));
+// console.log(operate('*', 3, 5));
+// console.log(operate('/', 3, 5));
+// console.log(operate('+', 3, 5));
