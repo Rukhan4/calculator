19a981b72eb997534aa4e4d0070a8f0cef83da91
Surur Khan <surur.rkhan@gmail.com>
2022-05-17 20:35:17 -0400

    Calculator functional but secondValue updating incorrectly

--- script.js
+++ script.js
@@ -2,26 +2,29 @@ let firstValue = "";
 let secondValue = "";
 let currentOperation = null;
 let resetScreenChoice = false;
+let startCalc = false;
+
 
 const numberBtns = document.querySelectorAll('[data-number]');
-const operationBtns = document.querySelectorAll('[data-operation]');
-const equalBtn = document.getElementById('equalsBtn')
+const operationBtns = document.querySelectorAll('[data-operator]');
+const equalBtn = document.getElementById('equalBtn')
 const clearBtn = document.getElementById('clearBtn')
 const deleteBtn = document.getElementById('deleteBtn')
 const pointBtn = document.getElementById('pointBtn')
 const lastOperationScreen = document.getElementById("lastOperationScreen")
 const currentOperationScreen = document.getElementById("currentOperationScreen")
 
-equalBtn.addEventListener('click', calculator);
+equalBtn.addEventListener('click', calculate);
 clearBtn.addEventListener('click', clear);
 deleteBtn.addEventListener('click', deleteNumber);
 pointBtn.addEventListener('click', placePoint);
 
 
 // FUNCTIONS TO PERFORM ACCESSORY OPERATIONS //
+
 function resetScreen() {
     currentOperationScreen.textContent = ''
-    shouldResetScreen = false;
+    resetScreenChoice = false;
 }
 
 function clear() {
@@ -41,6 +44,7 @@ function placePoint() {
     }
     if (!currentOperationScreen.textContent.includes('.')) {
         currentOperationScreen.textContent += '.';
+        firstValue += '.';
     }
 }
 
@@ -50,18 +54,54 @@ function deleteNumber() {
     } else {
         currentOperationScreen.textContent =
             currentOperationScreen.textContent.toString().slice(0, -1);
+        firstValue = currentOperationScreen.textContent;
     }
 }
 
 // FUNCTIONS TO PERFORM ARITHMETIC OPERATIONS //
 
+numberBtns.forEach((button) =>
+    button.addEventListener('click', () => updateNumber(button.textContent)));
+
+operationBtns.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent)));
+
 function updateNumber(num) {
-    if (currentOperationScreen.textContent === "0" || resetScreenChoice) {
+    if (resetScreenChoice) {
+        resetScreen();
+    } else if (currentOperationScreen.textContent === "0") {
         currentOperationScreen.textContent = num;
     } else {
         currentOperationScreen.textContent += num;
     }
 }
 
-numberBtns.forEach((button) =>
-    button.addEventListener('click', () => updateNumber(button.textContent)));
+function setOperation(operator) {
+    console.log(operator);
+    if (currentOperation !== null) {
+        calculate();
+    }
+    firstValue = currentOperationScreen.textContent;
+    console.log(firstValue);
+    lastOperationScreen.textContent = `${firstValue} ${operator}`;
+    currentOperation = operator;
+    resetScreenChoice = true;
+
+}
+
+function calculate() {
+    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+        alert("Really?");
+        clear();
+    } else {
+        secondValue = currentOperationScreen.textContent;
+        console.log(secondValue);
+        let result = operate(currentOperation, firstValue, secondValue);
+        currentOperationScreen.textContent = result;
+        lastOperationScreen.textContent = `${firstValue} ${currentOperation} ${secondValue} = ${result}`;
+        firstValue = result;
+        currentOperation = null;
+    }
+}
+
+clear();
